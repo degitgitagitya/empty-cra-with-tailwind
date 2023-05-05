822e3f11c36289b85a81bdcbe3c18805f9a51ae1
De Gitgit Agitya <de.agitya@prosa.ai>
2023-05-05 18:33:38 +0700

    Add congiuration for Chakra UI

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,12 @@
+import { Button } from '@chakra-ui/react'
+
 function App() {
-  return <h1 className='text-3xl font-bold underline'>Hello world!</h1>
+  return (
+    <h1 className='text-3xl font-bold underline'>
+      Hello world!
+      <Button>Hi</Button>
+    </h1>
+  )
 }
 
 export default App
